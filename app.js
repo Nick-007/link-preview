a83db5bb3f4b00a9488267eff48eb78c6a6ef9d4
Nick Boss <nicko.njuguna@gmail.com>
2020-06-14 01:56:01 +0000

    commit 6

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const linkPreview = require("link-preview");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+app.get('/:url', function (req, res) {
+  res.send(await linkPreview(req.params.url));
+});
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
 
-const previewData = await linkPreview(
-	"https://www.youtube.com/watch?v=8mqqY2Ji7_g"
-);
-console.log(previewData);
