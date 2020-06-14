3a3d3d20f73157ce57ba3371286935797aac87fe
Nick Boss <nicko.njuguna@gmail.com>
2020-06-14 00:47:03 +0000

    app start1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const linkPreview = require("link-preview");
 
-const previewData = await linkPreviewGenerator(
+const previewData = await linkPreview(
   "https://www.youtube.com/watch?v=8mqqY2Ji7_g"
 );
-console.log(previewData);
+alert(previewData);
