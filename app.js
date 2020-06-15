7bc35a9ae605fff759d5f8256bde31a01b1bee7f
Nick Boss <nicko.njuguna@gmail.com>
2020-06-15 15:47:34 +0000

    commit 9

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const linkPreview = require("link-preview");
+//const linkPreview = require("link-preview");
 const apl = express();
 const port = process.env.PORT || 8080;
 apl.get('/', function (req, res) {
-  const result = await linkPreview(req.query.url);
+  //const result = await linkPreview(req.query.url);
   res.send("Hello World");
 });
 apl.listen(port, function () {
