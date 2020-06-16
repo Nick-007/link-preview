6921f6935faebc87e6fe75394bb2615be5bf9027
Nick-007 <nicko.njuguna@gmail.com>
2020-06-16 04:56:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,11 @@ const port = process.env.PORT || 8080;
 
 app.get('/link', function (req, res) {
    (async function() {
-   //const response = await linkPreview("https://www.digitalocean.com/community/tutorials/nodejs-req-object-in-expressjs");
    const response = await linkPreview(req.query.url);
 	res.send(response);
   	})(req,res);
 });
-//const start = async function() {
-  // const result = await linkPreview("https://www.digitalocean.com/community/tutorials/nodejs-req-object-in-expressjs");
-  //  console.log(result);
-//}
+
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
   });
